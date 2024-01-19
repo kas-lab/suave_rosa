b43f1ec1de25fe51bc764ebb2f886e07ba652157
Gustavo <g.rezendesilva@tudelft.nl>
2024-01-19 18:39:13 -0300

    🔧 adjust launchfiles to new suave & rosa versions

--- launch/suave_rosa.launch.py
+++ launch/suave_rosa.launch.py
@@ -16,12 +16,32 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
+from launch.conditions import LaunchConfigurationEquals
+from launch.conditions import LaunchConfigurationNotEquals
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    mission_type = LaunchConfiguration('mission_type')
+    result_filename = LaunchConfiguration('result_filename')
+
+    mission_type_arg = DeclareLaunchArgument(
+        'mission_type',
+        default_value='time_constrained_mission',
+        description='Desired mission type' +
+                    '[time_constrained_mission or const_dist_mission]'
+    )
+
+    result_filename_arg = DeclareLaunchArgument(
+        'result_filename',
+        default_value='',
+        description='Name of the results file'
+    )
+
     pkg_rosa_kb = get_package_share_directory(
         'rosa_kb')
 
@@ -57,6 +77,7 @@ def generate_launch_description():
             'database_name': 'suave_db',
             'force_data': 'True',
             'force_database': 'True',
+            'infer': 'True',
         }.items()
     )
 
@@ -71,8 +92,41 @@ def generate_launch_description():
             'task_bridge': 'False'}.items()
     )
 
+    mission_config = os.path.join(
+        get_package_share_directory('suave_missions'),
+        'config',
+        'mission_config.yaml'
+    )
+
+    mission_metrics_node = Node(
+        package='suave_missions',
+        executable='mission_metrics',
+        name='mission_metrics',
+        parameters=[mission_config, {
+            'adaptation_manager': 'rosa',
+            'mission_name': mission_type,
+        }],
+        condition=LaunchConfigurationEquals('result_filename', '')
+    )
+
+    mission_metrics_node_override = Node(
+        package='suave_missions',
+        executable='mission_metrics',
+        name='mission_metrics',
+        parameters=[mission_config, {
+            'adaptation_manager': 'rosa',
+            'mission_name': mission_type,
+            'result_filename': result_filename,
+        }],
+        condition=LaunchConfigurationNotEquals('result_filename', '')
+    )
+
     return LaunchDescription([
+        mission_type_arg,
+        result_filename_arg,
         rosa_bringup,
         suave_rosa_bt_node,
-        suave_launch
+        suave_launch,
+        mission_metrics_node,
+        mission_metrics_node_override
     ])
